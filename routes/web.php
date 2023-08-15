91ea48eac0100dfd0f72335d97035cb0bc83fd94
basudev-g <flancer.basudev@gmail.com>
2023-08-15 22:50:18 +0600

    Readme file modified

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,6 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Admin Auth Routes
-
 Route::get('admin/home', [AdminController::class, 'index'])->name('admi.home');
 Route::get('admin', [LoginController::class, 'showLoginForm'])->name('admin.login');
 Route::post('admin', [LoginController::class, 'login']);
